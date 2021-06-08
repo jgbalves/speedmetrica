90f8a6a68223d50f9ddc1fe6ae350f798e53e80e
jgbalves <jgbalves.23@gmail.com>
2021-06-07 23:16:10 -0300

    created a button but not working yet

--- raceStrategyLaps/raceStrategyLapsIndex.js
+++ raceStrategyLaps/raceStrategyLapsIndex.js
@@ -1,1 +1,32 @@
 //alert("it works!");
+
+
+function addStints() {
+    var table = document.getElementById('raceStrategytable');
+    var rows = parseInt(document.getElementById('stintsNumber').value);
+    for (var i = 1; i <= rows; i++){
+        var tr = document.createElement('tr');
+        var cell1 = document.createElement('td');
+        var cell2 = document.createElement('td');
+        var cell3 = document.createElement('td');
+        var cell4 = document.createElement('td');
+        var cell5 = document.createElement('td');
+        var input2 = document.createElement('input');
+        var input3 = document.createElement('input');
+        var input4 = document.createElement('input');
+        var input5 = document.createElement('input');
+        input2.type = "number";
+        input3.type = "number";
+        input4.type = "number";
+        input5.type = "number";
+        input2.placeholder = 2;
+        cell1.innerHTML = i.toString();
+        cell2.appendChild(input2);
+        cell3.appendChild(input3);
+        cell4.appendChild(input4);
+        cell5.appendChild(input5);
+        table.appendChild(tr);        
+
+    }
+
+}
