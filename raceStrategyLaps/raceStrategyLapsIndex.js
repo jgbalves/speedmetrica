f00b41cfc2f9010b53087290a67a69b8925fdd42
jgbalves <jgbalves.23@gmail.com>
2021-06-09 00:20:41 -0300

    Fix stints button. Dropdown button doesnt work

--- raceStrategyLaps/raceStrategyLapsIndex.js
+++ raceStrategyLaps/raceStrategyLapsIndex.js
@@ -1,10 +1,14 @@
-//alert("it works!");
+//Gives Drivers names
+var drivers = ["Ninja", 'Letas', 'Jalves'];
 
+//Gives tire sets
+var tyreSets = ["Set 1", 'Set 2', 'Set 3'];
 
+//Add stints to Race Strategy table
 function addStints() {
     var table = document.getElementById('raceStrategyTable');
     var rows = parseInt(document.getElementById('stintsNumber').value);
-    for (var i = 1; i <= rows; i++) {
+    for (var n = 1; n <= rows; n++) {
         var tr = document.createElement('tr');
         var cell1 = document.createElement('td');
         var cell2 = document.createElement('td');
@@ -13,17 +17,27 @@ function addStints() {
         var cell5 = document.createElement('td');
         var input2 = document.createElement('input');
         var input3 = document.createElement('input');
-        var input4 = document.createElement('input');
-        var input5 = document.createElement('input');
+        var input4 = document.createElement('select');
+        var input5 = document.createElement('select');
         input2.type = "number";
         input3.type = "number";
-        input4.type = "number";
-        input5.type = "number";
-        cell1.innerHTML = i.toString();
+        cell1.innerHTML = n.toString();
         cell2.appendChild(input2); 
         cell3.appendChild(input3);
         cell4.appendChild(input4);
+        for (var i = 0; i < drivers.length; i++){
+            var option = document.createElement("option");
+            option.value = drivers[i];
+            option.text = drivers[i];
+            input4.appendChild(option);
+        }
         cell5.appendChild(input5);
+        for (var z = 0; z < tyreSets.length; z++){
+            var option = document.createElement("option");
+            option.value = tyreSets[i];
+            option.text = tyreSets[i];
+            input4.appendChild(option);
+        }
         tr.appendChild(cell1);
         tr.appendChild(cell2);
         tr.appendChild(cell3);
