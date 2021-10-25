9f505d47a25b29df50f6391dd5e4b62ca00eafed
jgbalves <jgbalves.23@gmail.com>
2021-10-25 20:01:41 -0300

    Laps were in string, converted to float

--- DataAnalysis/grip factors/scripts/grip_factor.py
+++ DataAnalysis/grip factors/scripts/grip_factor.py
@@ -28,30 +28,38 @@ def main():
     df = df.reset_index()
     df['G Force Lat'] = pd.to_numeric(df['G Force Lat'], downcast='float')
     df['G Force Long'] = pd.to_numeric(df['G Force Long'], downcast='float')
+    df['Ground Speed'] = pd.to_numeric(df['Ground Speed'], downcast='float')
+    df['Lap Distance'] = pd.to_numeric(df['Lap Distance'], downcast='float')
     df['Combined G'] = np.sqrt(df['G Force Lat'] ** 2 + df['G Force Long'] ** 2)
     df['Overall Grip Factor'] = [i > 1 for i in df['Combined G']]
     # df['Cornering Grip Factor'] = [i for i in df['Combined G'] if i > 0.5 in df['G Force Lat']]
     df['Cornering Grip Factor'] = np.where(df['G Force Lat'] > 0.5, df['Combined G'], np.nan)
     df['Braking Grip Factor'] = np.where(df['G Force Long'] < -1, df['Combined G'], np.nan)
     # df['Traction Grip Factor']= np.where([df['G Force Lat'] > 0.5 & df['G Force Long'] > 0], df['Combined G'], np.nan)
+
+    # # This part works (But I want columns to apply pivot table all at once)
+
     overall_grip_factor = df[(df['Combined G'] > 1)]['Combined G'].mean()
     cornering_grip_factor = df[(df['G Force Lat'] > 0.5)]['Combined G'].mean()
     braking_grip_factor = df[(df['G Force Long'] < -1)]['Combined G'].mean()
     traction_grip_factor = df[((df['G Force Lat'] > 0.5) & (df['G Force Long'] > 0))]['Combined G'].mean()
+    aero_grip_factor = df[((df['G Force Lat'] > 1) & (df['Ground Speed'] > 120))]['Combined G'].mean()
     print(overall_grip_factor)
     print(cornering_grip_factor)
     print(braking_grip_factor)
     print(traction_grip_factor)
+    print(aero_grip_factor)
 
     lap_num = [0]
 
-    for index in range(0, len(df['Lap Distance'])):
-        lap_num_act = lap_num[index]
+    for index in range(1, len(df['Lap Distance'])):
+        # lap_num_act = lap_num[index]
+        lap_num_prv = lap_num[index - 1]
         # import pdb; pdb.set_trace()
         if df['Lap Distance'][df.index[index - 1]] > df['Lap Distance'][df.index[index]]:
-            lap_num.append(lap_num_act + 1)
+            lap_num.append(lap_num_prv + 1)
         else:
-            lap_num.append(lap_num_act)
+            lap_num.append(lap_num_prv)
 
     print(lap_num)
 
