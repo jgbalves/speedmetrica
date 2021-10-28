081ddae6007f56b490981a2f7d49e09877122215
jgbalves <jgbalves.23@gmail.com>
2021-10-28 13:24:26 -0300

    first plot working

--- DataAnalysis/grip factors/scripts/SM_grip_factor.py
+++ DataAnalysis/grip factors/scripts/SM_grip_factor.py
@@ -16,7 +16,9 @@ import os
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 def grip_factor_tire_pressure(path):
@@ -26,11 +28,11 @@ def grip_factor_tire_pressure(path):
     outing_names = os.listdir(path)    # Fetch all files in path
     outing_names = [file for file in outing_names if '.csv' in file]    # Filtering the .csv
 
+    fig = go.Figure()
     # # loop for all outings
     for file in outing_names:
 
         outing_csv = file
-        # outing_path = Path(Path.home(), 'Github', 'speedmetrica', 'DataAnalysis', 'grip factors', 'csv outings', outing_csv)
         outing_csv = f'{outing_path}\{outing_csv}'
         outing = pd.read_csv(outing_csv, sep=',', low_memory=False, skiprows=13)
         df = outing
@@ -44,13 +46,17 @@ def grip_factor_tire_pressure(path):
         df['G Force Long'] = pd.to_numeric(df['G Force Long'], downcast='float')
         df['Ground Speed'] = pd.to_numeric(df['Ground Speed'], downcast='float')
         df['Lap Distance'] = pd.to_numeric(df['Lap Distance'], downcast='float')
+        df['Tire Press - FL'] = pd.to_numeric(df['Tire Press - FL'], downcast='float')
+        df['Tire Press - FR'] = pd.to_numeric(df['Tire Press - FR'], downcast='float')
+        df['Tire Press - RL'] = pd.to_numeric(df['Tire Press - RL'], downcast='float')
+        df['Tire Press - RR'] = pd.to_numeric(df['Tire Press - RR'], downcast='float')
 
         # # Cleaning data
         clean1 = df['G Force Long'] >= -2   # Due to pressing ESC in AMS the long G gets noise
         clean2 = df['G Force Long'] <= 2
         df = df[clean1 & clean2]
 
-        # # First try of column creation
+        # # Conditional columns creation
         df['Combined G'] = np.sqrt(df['G Force Lat'] ** 2 + df['G Force Long'] ** 2)    # ok
         df['Overall Grip Factor'] = np.where(df['Combined G'] > 1, df['Combined G'], np.nan)   # ok
         df['Cornering Grip Factor'] = np.where(df['G Force Lat'] > 0.5, df['Combined G'], np.nan)   # LatG > 0.5
@@ -101,13 +107,25 @@ def grip_factor_tire_pressure(path):
             'Cornering Grip Factor',
             'Braking Grip Factor',
             'Traction Grip Factor',
-            'Aero Grip Factor'
+            'Aero Grip Factor',
+            'Tire Press - FL',
+            'Tire Press - FR',
+            'Tire Press - RL',
+            'Tire Press - RR'
         ]
 
-        result = df.groupby(['Lap Number'])[columns].mean()
+        df2 = df.groupby(['Lap Number'])[columns].mean()
 
-        print(result)
+        print(df2)
 
-        fig = px.line(result)
-    fig.show()
+        fig.add_trace(go.Scatter(
+            x=df2['Tire Press - RR'],
+            y=df2['Cornering Grip Factor'],
+            mode='markers')
+        )
+
+        # plt.scatter(df2['Tire Press - RR'], df2['Cornering Grip Factor']) (work with matplotlib)
+
+    fig.show()    # Work with plotly
+    # plt.show()    # Work with matplotlib
 
