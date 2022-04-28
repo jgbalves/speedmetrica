3625b3e01a72df768492382e3ca57030c243baaa
Joao Alves <jgbalves.23@gmail.com>
2022-04-28 00:13:10 -0300

    dismembered plot in 3 plots containers

--- Application/f1_analysis/f1_analysis.py
+++ Application/f1_analysis/f1_analysis.py
@@ -3,63 +3,101 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 
-def get_f1_plot(year, track, session, driver_list):
-    '''random plot to check structure'''
+def car_data(year, track, chosen_session, driver):
+    session_plot = ff1.get_session(year, track, chosen_session)
+    session_plot.load()
+    fast_lap = session_plot.laps.pick_driver(driver).pick_fastest()
+    car_data = fast_lap.get_car_data()
+    car_data = car_data.add_distance(drop_existing=True)
+    return car_data
+
+
+def speed_plot(year, track, chosen_session, driver_list):
+    '''Speed x Distance comparison'''
 
     fig = make_subplots(
-    rows=3,
+    rows=1,
     cols=1,
-    subplot_titles=('Speed [km/h]', 'Throttle [%]', 'Delta [s]')
+    subplot_titles=('')
     )
     fig.update_layout(
-        title='Drivers Comparison',
+        title='Speed [km/h]',
         plot_bgcolor='rgb(230, 230,230)',
         showlegend=True
     )
 
     for driver in driver_list:
-
-        monza_quali = ff1.get_session(year, track, session)
-
-        laps = monza_quali.load_laps(with_telemetry=True)
-        fast_leclerc = laps.pick_driver(driver).pick_fastest()
-        lec_car_data = fast_leclerc.get_car_data()
-        lec_car_data = lec_car_data.add_distance(drop_existing=True)
-        print(list(lec_car_data.columns))
-        x_axle = lec_car_data['Distance']
-        y_axle = lec_car_data['Speed']
+        data_to_plot = car_data(year, track, chosen_session, driver)
 
         # The rest is just plotting
         fig.add_trace(
             go.Scatter(
-                x=x_axle,
-                y=y_axle,
+                x=data_to_plot['Distance'],
+                y=data_to_plot['Speed'],
                 mode='lines',
                 name=f'{driver}'
             ),
             row=1,
             col=1
         )
+    return fig
+
+def throttle_plot(year, track, chosen_session, driver_list):
+    '''Throttle x Distance comparison'''
+
+    fig = make_subplots(
+    rows=1,
+    cols=1,
+    subplot_titles=('')
+    )
+    fig.update_layout(
+        title='Throttle [%]',
+        plot_bgcolor='rgb(230, 230,230)',
+        showlegend=True
+    )
+
+    for driver in driver_list:
+        data_to_plot = car_data(year, track, chosen_session, driver)
+
+        # The rest is just plotting
         fig.add_trace(
             go.Scatter(
-                x=x_axle,
-                y=lec_car_data['Throttle'],
+                x=data_to_plot['Distance'],
+                y=data_to_plot['Throttle'],
                 mode='lines',
                 name=f'{driver}'
             ),
-            row=2,
+            row=1,
             col=1
         )
+    return fig
 
+def delta_plot(year, track, chosen_session, driver_list):    # Note: create a table with driver 1 - driver 2 = delta
+    '''delta to leader x Distance comparison'''
+
+    fig = make_subplots(
+    rows=1,
+    cols=1,
+    subplot_titles=('')
+    )
+    fig.update_layout(
+        title='Delta [s]',
+        plot_bgcolor='rgb(230, 230,230)',
+        showlegend=True
+    )
+    
+    for driver in driver_list:
+        data_to_plot = car_data(year, track, chosen_session, driver)
+
+        # The rest is just plotting
         fig.add_trace(
             go.Scatter(
-                x=x_axle,
-                y=lec_car_data['Time'],
+                x=data_to_plot['Distance'],
+                y=data_to_plot['Time'],
                 mode='lines',
                 name=f'{driver}'
             ),
-            row=3,
+            row=1,
             col=1
         )
-
     return fig
